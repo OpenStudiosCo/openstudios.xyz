884ba669766b937f6c1bd2e8e20faee234ad6a62
Paul Brzeski <mail@paulbrzeski.com>
2023-08-12 10:09:01 +0000

    Updating eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"src/virtual_office": "virtual_office"});
   eleventyConfig.addPassthroughCopy({"src/vendor": "vendor"});
 
-  eleventyConfig.addWatchTarget("./src/scripts/*");
+  eleventyConfig.addWatchTarget("./src/**/*");
 
   return {
     dir: {
