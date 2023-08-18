5e6eed35f241de771593444dd77831a9954c723c
Paul Brzeski <mail@paulbrzeski.com>
2023-08-18 13:13:01 +0000

    Migrating improvements to pokematrix from main matrix

--- src/scripts/pokematrix.js
+++ src/scripts/pokematrix.js
@@ -17,9 +17,6 @@ const canvas = document.getElementById('loader_symbols');
 canvas.style.mixBlendMode = "exclusion";
 const ctx = canvas.getContext('2d');
 
-const largeScreen = canvas.width * canvas.height < 2000000;
-const fontSize = largeScreen ? 15 : 8;
-
 const canvas2 = document.getElementById('loader_image');
 canvas2.style.display = 'none';
 const ctx2 = canvas2.getContext('2d', {
@@ -28,7 +25,12 @@ const ctx2 = canvas2.getContext('2d', {
 
 let w = canvas.width = canvas2.width = window.innerWidth;
 let h = canvas.height = canvas2.height = window.innerHeight;
-let cols = Math.floor(w / 20) + 1;
+
+const largeScreen = canvas.width * canvas.height < 2000000;
+const portraitMode = canvas.width < canvas.height;
+const fontSize = (portraitMode || largeScreen) ? 8 : 15;
+
+let cols = Math.floor(w / fontSize * 1.5) + 1;
 let ypos = Array(cols).fill(0);
 
 window.matrix_scene = {
@@ -45,17 +47,20 @@ window.matrix_scene = {
      */
     stage: 0,
     /**
+     * Timestamp when the current stage began.
+     */
+    stageStarted: 0,
+    /**
      * Stage specific variables.
      */
     // Stage 0
-    elapsed: 0, 
+    elapsed: 0,
     // Stage 1
-    loaded_done: 0, 
+    loaded_done: 0,
     loaded_target: 0,
     // Stage 2
-    transition_elapsed: 0,
     transition_total: 5000, // in milliseconds
-    
+
     // Animates the scene
     animate: function (currentTime) {
         const elapsedSinceLast = currentTime - window.matrix_scene.lastTime;
@@ -66,7 +71,7 @@ window.matrix_scene = {
 
         ctx.font = fontSize + 'pt monospace';
 
-        if ( window.matrix_scene.stage == 0 ) {
+        if (window.matrix_scene.stage == 0) {
             ctx.fillStyle = '#0001';
             ctx.fillRect(0, 0, w, h);
             // @todo: implement the intro sequence here - column reduce?
@@ -75,20 +80,19 @@ window.matrix_scene = {
 
                 window.matrix_scene.drawSymbol(y, ind);
 
-                if ( largeScreen ) {
+                if (largeScreen) {
                     window.matrix_scene.elapsed += Math.PI * 10;
-                }
-                else {
+                } else {
                     window.matrix_scene.elapsed += Math.PI;
                 }
-                
-                
+
+
                 if ((y > 1 + randomInt(1, window.matrix_scene.elapsed))) ypos[ind] = 0;
                 else ypos[ind] = y + fontSize;
 
             });
         }
-        if ( window.matrix_scene.stage == 1 ) {
+        if (window.matrix_scene.stage == 1) {
             ctx.fillStyle = "rgba(0,0,0,0.001)";
             ctx.fillRect(0, 0, w, h);
             ypos.forEach((y, ind) => {
@@ -97,111 +101,106 @@ window.matrix_scene = {
                 if (y > 1 + randomInt(1, 2000 * window.matrix_scene.loaded_done)) ypos[ind] = 0;
                 else ypos[ind] = y + fontSize;
 
-            });           
-            
+            });
+
         }
-        if ( window.matrix_scene.stage == 2 ) {
+        if (window.matrix_scene.stage == 2) {
             ctx.fillStyle = "rgba(0,0,0,0.001)";
             ctx.fillRect(0, 0, w, h);
 
             ypos.forEach((y, ind) => {
                 window.matrix_scene.drawSymbol(y, ind);
 
-                if ( canvas.width * canvas.height < 2000000 ) {
+                if (canvas.width * canvas.height < 2000000) {
                     window.matrix_scene.elapsed += Math.PI * 10;
-                }
-                else {
+                } else {
                     window.matrix_scene.elapsed += Math.PI;
                 }
-                
+
                 if ((y > 1 + randomInt(1, window.matrix_scene.elapsed))) ypos[ind] = 0;
                 else ypos[ind] = y + fontSize;
 
             });
 
         }
-        if ( window.matrix_scene.stage == 3 ) {
+        if (window.matrix_scene.stage == 3) {
             ypos.forEach((y, ind) => {
                 window.matrix_scene.drawSymbol(y, ind);
-                window.matrix_scene.elapsed += Math.PI;
-                
+
+                if (canvas.width * canvas.height < 2000000) {
+                    window.matrix_scene.elapsed += Math.PI * 10;
+                } else {
+                    window.matrix_scene.elapsed += Math.PI;
+                }
+
                 if ((y > 1 + randomInt(1, window.matrix_scene.elapsed))) ypos[ind] = 0;
                 else ypos[ind] = y + fontSize;
 
             });
         }
 
-        if ( ! window.matrix_scene.complete ) {
-            requestAnimationFrame( window.matrix_scene.animate );
+        if (!window.matrix_scene.complete) {
+            requestAnimationFrame(window.matrix_scene.animate);
         }
     },
-    drawSymbol: function( y, ind ) {
+    drawSymbol: function (y, ind) {
         const text = charArr[randomInt(0, charArr.length - 1)].toUpperCase();
-        const x = ind * (largeScreen ? 20 : 10);
+        const x = ind * fontSize * 1.5;
 
         ctx.fillStyle = getAverageColor(ctx2, x, y);
         ctx.fillText(text, x, y);
     },
     // Updates the scene
     updateStage: function () {
-        if ( window.matrix_scene.stage == 0 ) {
-            if ( ! window.virtual_office ) {
-                //requestAnimationFrame( window.matrix_scene.animate );
-            }
-            else {
+        if (window.matrix_scene.stage == 0) {
+            if (window.virtual_office && (Date.now() - window.matrix_scene.stageStarted > 500)) {
                 window.matrix_scene.stage = 1;
-                for ( var measure in window.virtual_office.loaders.stats ) {
+                window.matrix_scene.stageStarted = Date.now();
+                for (var measure in window.virtual_office.loaders.stats) {
                     window.matrix_scene.loaded_target += window.virtual_office.loaders.stats[measure].target;
                 }
 
                 const pageWrapper = document.getElementById('page-wrapper');
-                if(pageWrapper) {
+                if (pageWrapper) {
                     pageWrapper.style.opacity = 1;
                     pageWrapper.style.transition = 'opacity 1s';
                     pageWrapper.style.opacity = 0;
                 }
-                
+
             }
         }
-        if ( window.matrix_scene.stage == 1 ) {
+        if (window.matrix_scene.stage == 1) {
             window.matrix_scene.loaded_done = 0;
-            for ( var measure in window.virtual_office.loaders.stats ) {
+            for (var measure in window.virtual_office.loaders.stats) {
                 window.matrix_scene.loaded_done += window.virtual_office.loaders.stats[measure].loaded;
             }
 
             if (
-                window.matrix_scene.loaded_done < window.matrix_scene.loaded_target
+                window.matrix_scene.loaded_done == window.matrix_scene.loaded_target && (Date.now() - window.matrix_scene.stageStarted > 500)
             ) {
-                // requestAnimationFrame( window.matrix_scene.animate );
-            }
-            else {
                 window.matrix_scene.stage = 2;
+                window.matrix_scene.stageStarted = Date.now();
             }
         }
-        if ( window.matrix_scene.stage == 2 ) {
+        if (window.matrix_scene.stage == 2) {
             if (
-                window.matrix_scene.elapsed < 80000
+                (Date.now() - window.matrix_scene.stageStarted > 500)
             ) {
-                //requestAnimationFrame( window.matrix_scene.animate );
-            }
-            else {
                 window.matrix_scene.stage = 3;
+                window.matrix_scene.stageStarted = Date.now();
                 canvas.style.transition = 'filter 5s, transform 5s';
                 canvas.style.transform = "scale(5)";
                 canvas.style.filter = "blur(5px)";
 
-                webgl.style.transition = 'filter 3s 2s, opacity 2s';
+                webgl.style.transition = 'filter 3s 2s, opacity 4s';
                 webgl.style.filter = "saturate(1)";
                 webgl.style.opacity = 1;
             }
         }
-        if ( window.matrix_scene.stage == 3 ) {
+        if (window.matrix_scene.stage == 3) {
             if (
-                (window.matrix_scene.transition_elapsed < window.matrix_scene.transition_total)
+                (Date.now() - window.matrix_scene.stageStarted) > window.matrix_scene.transition_total
             ) {
-                window.matrix_scene.transition_elapsed += 100;
-            }
-            else {
                 clearInterval(window.matrix_scene.interval);
                 window.matrix_scene.complete = true;
             }
@@ -211,14 +210,15 @@ window.matrix_scene = {
     start: function () {
         ctx.fillStyle = '#0001';
         ctx.fillRect(0, 0, w, h);
-        
-        //window.addEventListener('orientationchange', handleViewportChange);
-        //window.addEventListener('resize', handleViewportChange);
-        
-        // Hide body element scrollbars as the 3D viewport takes over.
-        //document.querySelector("body").style.overflow = 'hidden';
-        //window.matrix_scene.interval = setInterval( window.matrix_scene.updateStage, 100);
-        //requestAnimationFrame( window.matrix_scene.animate );
+
+        // window.addEventListener('orientationchange', handleViewportChange);
+        // window.addEventListener('resize', handleViewportChange);
+
+        // // Hide body element scrollbars as the 3D viewport takes over.
+        // document.querySelector("body").style.overflow = 'hidden';
+        // window.matrix_scene.stageStarted = Date.now();
+        // window.matrix_scene.interval = setInterval(window.matrix_scene.updateStage, 100);
+        // requestAnimationFrame(window.matrix_scene.animate);
     }
 };
 
@@ -270,7 +270,7 @@ function randomInt(min, max) {
 function handleViewportChange() {
     w = canvas.width = canvas2.width = window.innerWidth;
     h = canvas.height = canvas2.height = window.innerHeight;
-    cols = Math.floor(w / 20) + 1;
+    cols = Math.floor(w / fontSize * 1.5) + 1;
     ypos = Array(cols).fill(0);
 
     ctx.fillStyle = '#000';
