0094410e0fcb31ea965135f9be618f0fc09050be
Paul Brzeski <mail@paulbrzeski.com>
2024-04-26 15:02:33 +0000

    #28 - Tidying and small improvements

--- src/app/effects.js
+++ src/app/effects.js
@@ -1,11 +1,7 @@
 import * as THREE from 'three';
 
-import { HalfFloatType } from "three";
-
-
 import {
   // Core stuff
-  BlendFunction,
   EffectComposer,
   EffectPass,
   RenderPass,
@@ -14,14 +10,15 @@ import {
   SelectiveBloomEffect,
 
   // Anti aliasing
+  EdgeDetectionMode,
   SMAAEffect,
 	SMAAPreset,
-	EdgeDetectionMode,
   
   // SSAO
-  SSAOEffect,
+  BlendFunction,
   DepthDownsamplingPass,
   NormalPass,
+  SSAOEffect,
   TextureEffect,
 
   // Tone Mapping
@@ -29,14 +26,10 @@ import {
 	ToneMappingMode
 } from "postprocessing";
 
-
-// @todo: Replace with https://pmndrs.github.io/postprocessing/public/demo/#tone-mapping
-import { OutputPass } from 'three/addons/postprocessing/OutputPass.js';
-
 // Sets up the effects
 export function setupEffects( ) {
   const composer = new EffectComposer(window.virtual_office.renderers.webgl, {
-    frameBufferType: HalfFloatType
+    frameBufferType: THREE.HalfFloatType
   });
   composer.addPass(new RenderPass(window.virtual_office.scene, window.virtual_office.camera));
 
@@ -123,59 +116,4 @@ export function setupEffects( ) {
   composer.addPass(new EffectPass(window.virtual_office.camera, smaa, ssao, textureEffect, bloom, toneMappingEffect));
   
   window.virtual_office.effects = composer;
-}
-
-export function setupEffectsOld( ) {
-  // Apply Unreal Bloom post-processing effect
-  var renderScene = new RenderPass(  window.virtual_office.scene, window.virtual_office.camera);
-
-  window.virtual_office.effects.main = new EffectComposer(window.virtual_office.renderers.webgl);
-  window.virtual_office.effects.main.setSize(window.innerWidth, window.innerHeight);
-  window.virtual_office.effects.main.addPass(renderScene);
-
-  window.virtual_office.renderers.webgl.shadowMap.enabled = true;
-  window.virtual_office.renderers.webgl.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
-
-  const ssaoPass = new SSAOPass( window.virtual_office.scene, window.virtual_office.camera, window.innerWidth, window.innerHeight );
-  ssaoPass.kernelRadius = 20;
-  ssaoPass.output = SSAOPass.OUTPUT.Beauty;
-  window.virtual_office.effects.main.addPass( ssaoPass );
-
-  const ssaaPass = new SSAARenderPass ( window.virtual_office.scene, window.virtual_office.camera );
-  ssaaPass.sampleLevel = 1;
-  window.virtual_office.effects.main.addPass( ssaaPass );
-    
-  window.virtual_office.effects.bloomLayer = new THREE.Layers();
-  window.virtual_office.effects.bloomLayer.set( 1 );
-
-  const bloomPass = new UnrealBloomPass(new THREE.Vector2(window.innerWidth, window.innerHeight), 1.5, 0.4, 0.85);
-  bloomPass.threshold = 0.15;
-  bloomPass.strength = .85;
-  bloomPass.radius = 0.85;
-
-  window.virtual_office.effects.bloom = new EffectComposer( window.virtual_office.renderers.webgl );
-  window.virtual_office.effects.bloom.setSize(window.innerWidth, window.innerHeight);
-  window.virtual_office.effects.bloom.renderToScreen = false;
-  window.virtual_office.effects.bloom.addPass( renderScene );
-  window.virtual_office.effects.bloom.addPass( bloomPass );
-
-  const mixPass = new ShaderPass(
-    new THREE.ShaderMaterial( {
-      uniforms: {
-        baseTexture: { value: null },
-        bloomTexture: { value: window.virtual_office.effects.bloom.renderTarget2.texture }
-      },
-      vertexShader: document.getElementById( 'vertexshader' ).textContent,
-      fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
-      defines: {}
-    } ), 'baseTexture'
-  );
-  mixPass.needsSwap = true;
-
-  window.virtual_office.effects.main.addPass( mixPass );
-
-  const tonePass = new OutputPass(THREE.ACESFilmicToneMapping);
-  tonePass.toneMappingExposure = Math.pow(Math.PI / 3, 4.0);
-  window.virtual_office.effects.main.addPass( tonePass );
-
 }
