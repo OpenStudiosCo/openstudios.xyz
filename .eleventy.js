64b1f41f26de6dbf303de7237ddd7c32204608d1
Paul Brzeski <mail@paulbrzeski.com>
2024-06-25 17:40:07 +0000

    #31 - Adding Parvus and next set of services examples

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,21 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({"src/assets": "assets"});
   eleventyConfig.addPassthroughCopy({"src/vendor": "vendor"});
+  
+  // Keen Slider static includes.
   eleventyConfig.addPassthroughCopy({
     "./node_modules/keen-slider/keen-slider.js": "./assets/js/keen-slider.js",
   });
   eleventyConfig.addPassthroughCopy({
     "./node_modules/keen-slider/keen-slider.min.css": "./assets/css/keen-slider.min.css",
+  });
+
+  // Parvus static includes.
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/parvus/dist/css/parvus.min.css": "./assets/css/parvus.min.css",
+  });
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/parvus/dist/js/parvus.min.js": "./assets/js/parvus.min.js",
   });
 
   eleventyConfig.addPassthroughCopy("CNAME");
