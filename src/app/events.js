5ff4cb1649697e930744472b3f18a4b2fb6dadb9
Paul Brzeski <mail@paulbrzeski.com>
2024-09-28 16:30:41 +0000

    #29 - Formatting

--- src/app/events.js
+++ src/app/events.js
@@ -21,26 +21,26 @@ export async function handleViewportChange() {
   var width = window.innerWidth;
   var height = window.innerHeight;
 
-  window.virtual_office.renderers.webgl.setSize(width, height);
+  window.virtual_office.renderers.webgl.setSize( width, height );
 
-  if (window.virtual_office.effects && window.virtual_office.effects.passes.length > 0)
-    window.virtual_office.effects.setSize(width, height);
+  if ( window.virtual_office.effects && window.virtual_office.effects.passes.length > 0 )
+    window.virtual_office.effects.setSize( width, height );
 
   window.virtual_office.camera.aspect = width / height;
 
-  window.virtual_office.camera.fov = setCameraFOV(window.virtual_office.camera.aspect);
-  if (!window.virtual_office.selected &&  ! window.virtual_office.moving) {
+  window.virtual_office.camera.fov = setCameraFOV( window.virtual_office.camera.aspect );
+  if ( !window.virtual_office.selected && !window.virtual_office.moving ) {
     let posZ = -20;
-    window.virtual_office.camera.position.z = posZ + (window.virtual_office.room_depth / 2);
-    window.virtual_office.camera.rotation.x = - (Math.PI / 30) * window.virtual_office.camera.aspect;
+    window.virtual_office.camera.position.z = posZ + ( window.virtual_office.room_depth / 2 );
+    window.virtual_office.camera.rotation.x = - ( Math.PI / 30 ) * window.virtual_office.camera.aspect;
   }
   window.virtual_office.camera.updateProjectionMatrix();
-  
+
   const newRoom = await createOfficeRoom();
   window.virtual_office.scene_objects.room.geometry = newRoom.geometry;
 
-  if ( ! window.virtual_office.started ) {
-    if (window.virtual_office.camera.aspect < 0.88) {
+  if ( !window.virtual_office.started ) {
+    if ( window.virtual_office.camera.aspect < 0.88 ) {
       window.virtual_office.settings.startPosZ = -5;
     }
     else {
@@ -48,39 +48,39 @@ export async function handleViewportChange() {
     }
   }
   else {
-    
-    // Adjust desk positions based on the aspect ratio
-    window.virtual_office.scene_objects.deskGroup.children.forEach(function (mesh, i) {
-      if ( mesh.name == 'desk') {
-        updateDeskZ(mesh, mesh.deskIndex);
 
-        updateDeskZ(mesh.webGLScreen, mesh.deskIndex);
+    // Adjust desk positions based on the aspect ratio
+    window.virtual_office.scene_objects.deskGroup.children.forEach( function ( mesh, i ) {
+      if ( mesh.name == 'desk' ) {
+        updateDeskZ( mesh, mesh.deskIndex );
+
+        updateDeskZ( mesh.webGLScreen, mesh.deskIndex );
 
         mesh.webGLScreen.position.z += .175;
 
       }
 
-      if ( mesh.name == 'chair') {
-        updateDeskZ(mesh, mesh.deskIndex);
+      if ( mesh.name == 'chair' ) {
+        updateDeskZ( mesh, mesh.deskIndex );
       }
-    });
+    } );
 
     let backWallZ = - 15 - window.virtual_office.room_depth / 2;
     window.virtual_office.scene_objects.wallGroup.position.z = backWallZ;
-    window.virtual_office.scene_objects.tvWebGL.position.z =  backWallZ +1;
+    window.virtual_office.scene_objects.tvWebGL.position.z = backWallZ + 1;
 
-    window.virtual_office.scene_objects.door.position.z = - 15 + (window.virtual_office.room_depth / 2);
-    window.virtual_office.scene_objects.door_frame.position.z = - 15 + (window.virtual_office.room_depth / 2);
+    window.virtual_office.scene_objects.door.position.z = - 15 + ( window.virtual_office.room_depth / 2 );
+    window.virtual_office.scene_objects.door_frame.position.z = - 15 + ( window.virtual_office.room_depth / 2 );
 
     updateBlogLayout( window.virtual_office.scene_objects.blogWall );
 
-    window.virtual_office.scene_objects.blogWall.position.z = - 15 - ((window.virtual_office.room_depth / 8) * 1.5);
+    window.virtual_office.scene_objects.blogWall.position.z = - 15 - ( ( window.virtual_office.room_depth / 8 ) * 1.5 );
 
     // Update camera position if viewing corkboard.
     if ( window.virtual_office.selected.name == 'corkBoard' ) {
-      let [ targetPosition, targetRotation ] = window.virtual_office.scene_objects.blogWall.getViewingCoords( );
+      let [ targetPosition, targetRotation ] = window.virtual_office.scene_objects.blogWall.getViewingCoords();
 
-      window.virtual_office.camera.position.copy(targetPosition);
+      window.virtual_office.camera.position.copy( targetPosition );
     }
 
     if ( window.virtual_office.scene_objects.blog_sign ) {
@@ -90,48 +90,48 @@ export async function handleViewportChange() {
   }
 }
 
-export function handleInteractions( ) {
+export function handleInteractions() {
   // update the picking ray with the camera and pointer position
-  window.virtual_office.raycaster.setFromCamera(new THREE.Vector2(window.virtual_office.pointer.x, window.virtual_office.pointer.y), window.virtual_office.camera);
+  window.virtual_office.raycaster.setFromCamera( new THREE.Vector2( window.virtual_office.pointer.x, window.virtual_office.pointer.y ), window.virtual_office.camera );
 
   // calculate objects intersecting the picking ray
-  const intersects = window.virtual_office.raycaster.intersectObjects(window.virtual_office.scene.children);
+  const intersects = window.virtual_office.raycaster.intersectObjects( window.virtual_office.scene.children );
 
-  if (intersects.length > 0) {  
-    for (let i = 0; i < intersects.length; i++) {
+  if ( intersects.length > 0 ) {
+    for ( let i = 0; i < intersects.length; i++ ) {
 
       // If nothing is selected, allow hover effects.
-      if (!window.virtual_office.selected) {
+      if ( !window.virtual_office.selected ) {
 
         // Clear the hovered object.
-        window.virtual_office.hovered = intersects[i].object;
-      
+        window.virtual_office.hovered = intersects[ i ].object;
+
         document.documentElement.style.cursor = "default";
 
-        if (intersects[i].object.name == "screen" || intersects[i].object.name == "desk_part" || intersects[i].object.name == "desk_label") {
+        if ( intersects[ i ].object.name == "screen" || intersects[ i ].object.name == "desk_part" || intersects[ i ].object.name == "desk_label" ) {
           document.documentElement.style.cursor = "pointer";
-    
-          screenHover(intersects[i].object.parent);
-    
+
+          screenHover( intersects[ i ].object.parent );
+
           break;
         }
 
-        if (intersects[i].object.name == "blog_sign" || intersects[i].object.name == "corkBoard"  || intersects[i].object.name == "corkboardMesh") {
+        if ( intersects[ i ].object.name == "blog_sign" || intersects[ i ].object.name == "corkBoard" || intersects[ i ].object.name == "corkboardMesh" ) {
           document.documentElement.style.cursor = "pointer";
 
           blogHover();
-    
+
           break;
         }
-    
-        if (intersects[i].object.name == "neon_sign" || intersects[i].object.name == "tv") {
+
+        if ( intersects[ i ].object.name == "neon_sign" || intersects[ i ].object.name == "tv" ) {
           document.documentElement.style.cursor = "pointer";
 
-          screenHover(intersects[i].object.parent);
-    
+          screenHover( intersects[ i ].object.parent );
+
           break;
         }
-    
+
       }
       // Otherwise we're only tracking interaction with the exit sign.
       else {
@@ -140,13 +140,13 @@ export function handleInteractions( ) {
           // Remove previous title.
           clearBlogWallTitle();
 
-          if (intersects[i].object.name == "polaroid") {
-            window.virtual_office.hovered = intersects[i].object;
+          if ( intersects[ i ].object.name == "polaroid" ) {
+            window.virtual_office.hovered = intersects[ i ].object;
 
             document.documentElement.style.cursor = "pointer";
-  
-            polaroidHover( intersects[i].object );
-      
+
+            polaroidHover( intersects[ i ].object );
+
             break;
           }
           else {
@@ -155,7 +155,7 @@ export function handleInteractions( ) {
         }
         else {
           document.documentElement.style.cursor = "inherit";
-          if ( ! window.virtual_office.hovered ) {
+          if ( !window.virtual_office.hovered ) {
             window.virtual_office.hovered = window.virtual_office.selected;
           }
         }
@@ -172,16 +172,16 @@ function polaroidHover( polaroid ) {
    * Set the title of the cork board.
    */
   // Setup current title and set scale to 10%.
-  window.virtual_office.scene_objects.blog_selected_title = polaroid.getObjectByName('polaroid_label').clone({recursive: true});
+  window.virtual_office.scene_objects.blog_selected_title = polaroid.getObjectByName( 'polaroid_label' ).clone( { recursive: true } );
   window.virtual_office.scene_objects.blog_selected_title.scale.setScalar( .1 );
-  
+
   // Set material to neon blue.
   window.virtual_office.scene_objects.blog_selected_title.material = new THREE.MeshPhongMaterial( { color: 0xffffff, emissive: 0x00EEff, emissiveIntensity: 0.5 } );
   window.virtual_office.scene_objects.blog_selected_title.visible = true;
-  
+
   // Set position, x based on half width.
   let meshWidth = getMeshWidth( window.virtual_office.scene_objects.blog_selected_title ) * .1;
-  window.virtual_office.scene_objects.blog_selected_title.position.set( - meshWidth / 2, window.virtual_office.camera.aspect >= 0.88 ? 0 : 6.5 , .1 );
+  window.virtual_office.scene_objects.blog_selected_title.position.set( - meshWidth / 2, window.virtual_office.camera.aspect >= 0.88 ? 0 : 6.5, .1 );
 
   // Add selected title to blog wall.
   window.virtual_office.scene_objects.blogWall.add( window.virtual_office.scene_objects.blog_selected_title );
@@ -189,16 +189,16 @@ function polaroidHover( polaroid ) {
   /**
    * Handle click.
    */
-  if (window.virtual_office.pointer.z && !window.virtual_office.moving) {
-    if (window.virtual_office.selected.name == 'corkBoard') {
+  if ( window.virtual_office.pointer.z && !window.virtual_office.moving ) {
+    if ( window.virtual_office.selected.name == 'corkBoard' ) {
       window.virtual_office.moving = true;
       window.virtual_office.selected = polaroid;
 
       // Start loading the screen.
-      document.getElementById('pageOverlay').src = polaroid.userData.url;
+      document.getElementById( 'pageOverlay' ).src = polaroid.userData.url;
       document.title = polaroid.userData.title;
 
-      history.pushState({}, "", polaroid.userData.url.replace('iframes/', ''));
+      history.pushState( {}, "", polaroid.userData.url.replace( 'iframes/', '' ) );
       stretchSelectedScreen();
       window.virtual_office.pointer.z = 0;
       clearBlogWallTitle();
@@ -210,18 +210,18 @@ function blogHover() {
   /**
    * Handle click.
    */
-  if (window.virtual_office.pointer.z && !window.virtual_office.moving) {
-    if (!window.virtual_office.selected) {
+  if ( window.virtual_office.pointer.z && !window.virtual_office.moving ) {
+    if ( !window.virtual_office.selected ) {
       window.virtual_office.moving = true;
       window.virtual_office.selected = window.virtual_office.scene_objects.blogWall;
 
-      let [ targetPosition, targetRotation ] = window.virtual_office.scene_objects.blogWall.getViewingCoords( );
+      let [ targetPosition, targetRotation ] = window.virtual_office.scene_objects.blogWall.getViewingCoords();
 
-      window.virtual_office.tweens.rotateCamera.to({ x: targetRotation.x, y: targetRotation.y, z: targetRotation.z }, 1000).start();
-      window.virtual_office.tweens.moveCamera.to(targetPosition, 1000).onComplete(()=>{
+      window.virtual_office.tweens.rotateCamera.to( { x: targetRotation.x, y: targetRotation.y, z: targetRotation.z }, 1000 ).start();
+      window.virtual_office.tweens.moveCamera.to( targetPosition, 1000 ).onComplete( () => {
         // Show the exit sign.
-        document.getElementById('exitSign').style.display = 'block';
-      }).start();
+        document.getElementById( 'exitSign' ).style.display = 'block';
+      } ).start();
 
     }
 
@@ -232,21 +232,21 @@ function screenHover( screen ) {
   /**
    * Handle click.
    */
-  if (window.virtual_office.pointer.z && !window.virtual_office.moving) {
-    if (!window.virtual_office.selected) {
+  if ( window.virtual_office.pointer.z && !window.virtual_office.moving ) {
+    if ( !window.virtual_office.selected ) {
       window.virtual_office.moving = true;
       window.virtual_office.selected = screen;
 
-      let [ targetPosition, targetRotation ] = screen.webGLScreen.getViewingCoords( );
+      let [ targetPosition, targetRotation ] = screen.webGLScreen.getViewingCoords();
 
       // Start loading the screen.
-      document.getElementById('pageOverlay').src = window.virtual_office.selected.webGLScreen.pageUrl;
-      document.title =  window.virtual_office.selected.webGLScreen.pageTitle + ' | Open Studios | Perth, Western Australia';
+      document.getElementById( 'pageOverlay' ).src = window.virtual_office.selected.webGLScreen.pageUrl;
+      document.title = window.virtual_office.selected.webGLScreen.pageTitle + ' | Open Studios | Perth, Western Australia';
 
-      history.pushState({}, "", window.virtual_office.selected.webGLScreen.pageRealUrl);
+      history.pushState( {}, "", window.virtual_office.selected.webGLScreen.pageRealUrl );
 
-      window.virtual_office.tweens.rotateCamera.to({ x: targetRotation.x, y: targetRotation.y, z: targetRotation.z }, 1000).start();
-      window.virtual_office.tweens.moveCamera.to(targetPosition, 1000).onComplete(stretchSelectedScreen).start();
+      window.virtual_office.tweens.rotateCamera.to( { x: targetRotation.x, y: targetRotation.y, z: targetRotation.z }, 1000 ).start();
+      window.virtual_office.tweens.moveCamera.to( targetPosition, 1000 ).onComplete( stretchSelectedScreen ).start();
 
     }
 
@@ -257,22 +257,22 @@ export function handleExitSign() {
 
   window.virtual_office.started = true;
 
-  document.title =  'Open Studios | Perth, Western Australia';
-  history.pushState({}, "", '/');
-  
-  window.virtual_office.moving = true;
-  var targetRotation = - (Math.PI / 30) * window.virtual_office.camera.aspect;
+  document.title = 'Open Studios | Perth, Western Australia';
+  history.pushState( {}, "", '/' );
 
-  let cameraDefaultPosition = { x: 0, y: 18, z: -20 + (window.virtual_office.room_depth / 2) },
+  window.virtual_office.moving = true;
+  var targetRotation = - ( Math.PI / 30 ) * window.virtual_office.camera.aspect;
+
+  let cameraDefaultPosition = { x: 0, y: 18, z: -20 + ( window.virtual_office.room_depth / 2 ) },
     cameraDefaultRotation = { x: targetRotation, y: 0, z: 0 };
 
   // Animate the camera resetting from any other position.
   resetReusables();
   window.virtual_office.tweens.resetCameraRotation.start();
-  window.virtual_office.tweens.resetCameraPosition.onStart(shrinkScreenBack).start();
+  window.virtual_office.tweens.resetCameraPosition.onStart( shrinkScreenBack ).start();
 
   window.virtual_office.selected = false;
-  
+
 }
 
 /**
@@ -282,21 +282,21 @@ export function handleExitSign() {
 // Function to update the CSS object's size to fit the visible space
 function stretchSelectedScreen() {
 
-  document.getElementById('pageOverlay').style.display = 'block';
-  
-  document.getElementById('exitSign').style.display = 'block';
+  document.getElementById( 'pageOverlay' ).style.display = 'block';
+
+  document.getElementById( 'exitSign' ).style.display = 'block';
 
   window.virtual_office.started = false;
 }
 
 // Restore the CSS object to its original size.
 function shrinkScreenBack() {
-  
+
   // Reset scroll position of the iFrame
-  document.getElementById('pageOverlay').contentWindow.scrollTo( 0 , 0 );
+  document.getElementById( 'pageOverlay' ).contentWindow.scrollTo( 0, 0 );
 
-  document.getElementById('pageOverlay').style.display = 'none';
+  document.getElementById( 'pageOverlay' ).style.display = 'none';
 
-  document.getElementById('exitSign').style.display = 'none';  
+  document.getElementById( 'exitSign' ).style.display = 'none';
 
 }
